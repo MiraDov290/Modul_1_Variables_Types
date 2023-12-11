c8636779caecd701e7afb9e75aba618755e952ef
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-12-11 15:07:00 +0000

    changs

--- script2.js
+++ script2.js
@@ -8,10 +8,78 @@
 
 
 
-const str = 'abc';
-for (let i = 0; i < str.length; i += 1){
-    if (str[i] === 'b') {
-        break
-    }
-    console.log(str[i]);//a
-}
+// const str = 'abc';
+// for (let i = 0; i < str.length; i += 1){
+//     if (str[i] === 'b') {
+//         break
+//     }
+//     console.log(str[i]);//a
+// }
+
+
+// if (4 === 4) {
+//     console.log('true');//true, якщо відповідає умові, елемент успішний
+// }
+
+
+
+// if (2 % 2 === 0) {
+//     console.log('парне');//парне
+// } else {
+//     console.log("не парне");
+// }
+
+
+
+// if (3 % 2 === 0) {//або (!(4%2))
+//     console.log('парне');
+// } else {
+//     console.log("не парне");//не парне
+// }
+
+
+
+// if (4 < 3) {
+//     console.log('YES');
+// } else if (4 < 6) {
+//     console.log('YES 1');//YES 1
+// } else if(4<7){
+//     console.log('YES 2');
+// }else if(4<8){
+// console.log('YES 3');
+// } else {
+//     console.log('NO');
+// }
+
+
+
+
+
+// const result = 4 % 2 === 0 ? 'Парне' : 'Не парне';
+// console.log(result);//Парне
+
+
+
+
+
+// const points = 100;
+// let partner;
+// switch (points) {
+//     case 50:
+//         partner = 'Base';
+//         break;
+//     case 75:
+//         partner = 'Bronze';
+//         break;
+//     case 100:
+//         partner = 'Gold';
+//         break;
+//     default:
+//         console.log('Another value');
+// }
+// console.log(partner);//100
+
+
+
+
+
